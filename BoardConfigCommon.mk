5c9efaf6541a8474cc83e70f781de3600ea77536
Humberto Borba <humberos@gmail.com>
2013-12-26 14:23:26 -0200

    bring up omniROM
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,12 +20,18 @@
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/n80xx-common/include
 
+# Exynos4x12 Tablet
+BOARD_EXYNOS4X12_TABLET := true
+
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6262
 
 # Camera
 BOARD_USES_PROPRIETARY_LIBCAMERA := true
+
+# Needed for camera blob to work after https://gerrit.omnirom.org/#/c/3005
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
 # Filesystem
 BOARD_NAND_PAGE_SIZE := 2048
@@ -40,3 +46,24 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Recovery
 # inherit from the proprietary version
 -include vendor/samsung/n80xx-common/BoardConfigVendor.mk
+
+# TWRP
+
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Recovery: set depending on recovery being built for. (CWM or TWRP)
+#           both init scripts can be found in the recovery folder
+TARGET_RECOVERY_INITRC := device/samsung/n80xx-common/recovery/init.rc
+
+# TWRP specific build flags
+DEVICE_RESOLUTION := 1280x800
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sdcard"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sdcard"
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+TW_NO_USB_STORAGE := true
+TWRP_EVENT_LOGGING := true
